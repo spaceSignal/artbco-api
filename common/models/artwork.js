fce53c313d2d768d7824304d2c543b69d0e32756
rudyg <rudyg@bgtgcorp.com>
2015-09-06 23:18:33 -0400

    changes

--- common/models/artwork.js
+++ common/models/artwork.js
@@ -1,8 +1,6 @@
 module.exports = function (Artwork) {
     Artwork.validatesPresenceOf(
-        'name',
-        'salesPrice',
-        'leasePrice'
+        'name'
     );
 
     Artwork.validatesLengthOf('name', {
@@ -10,16 +8,5 @@ module.exports = function (Artwork) {
         message: {
             min: 'name is too short'
         }
-    });
-
-    var validStatuses = ['sale', 'lease', 'sold', 'leased', 'unkwon'];
-    var validShapes = ['square', 'circle', 'pentagon'];
-
-    Artwork.validatesInclusionOf('status', {
-        in: validStatuses
-    });
-
-    Artwork.validatesInclusionOf('shape', {
-        in: validShapes
     });
 };
