9fdd06f28b92428c58006aca37fe3d898fef7376
rudyg <rudyg@bgtgcorp.com>
2015-09-07 01:07:55 -0400

    correction

--- common/models/artwork.js
+++ common/models/artwork.js
@@ -1,6 +1,6 @@
 module.exports = function (Artwork) {
 
 Artwork.validatesUniquenessOf('serialNumber', {message: 'the serial number is not unique'});
-Artwork.validatesInclusionOf('status', {in: ['status1', 'status2', 'status3']});
+Artwork.validatesInclusionOf('status', {in: ['active', 'inactive', 'hold']});
 
 };
