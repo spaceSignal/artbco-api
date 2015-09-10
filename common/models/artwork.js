f1c5bd6cf37c4bb7d7ca67b5c5025be00c8e2b55
rudyg <rudyg@bgtgcorp.com>
2015-09-10 01:15:58 -0400

    finalize api
    finished Collection and Artwork endpoints

--- common/models/artwork.js
+++ common/models/artwork.js
@@ -3,91 +3,198 @@ module.exports = function (Artwork) {
 Artwork.validatesUniquenessOf('serialNumber', {message: 'the serial number is not unique'});
 Artwork.validatesInclusionOf('status', {in: ['active', 'inactive', 'hold']});
 
-// hold
-var details = undefined;
-var imgs = undefined;
-var callback = undefined;
+  // hold
+  var details = undefined;
+  var imgs = undefined;
+  var callback = undefined;
+  var sameArtist = undefined;
+  var sameArt = undefined;
+  var skipIt = undefined;
 
 
-    Artwork.detailView = function(artworkId, cb) {
+  Artwork.detailView = function(artworkId, cb) {
 
-        // load up cb only the first time
-        callback = typeof callback == 'undefined' ? cb : callback;
+    // load up cb only the first time
+    callback = typeof callback == 'undefined' ? cb : callback;
 
-        Artwork.getDetailsImgs(artworkId);
+    Artwork.getDetailsImgs(artworkId);
 
+  }
+
+  Artwork.sameArt = function(artworkId, skip, cb) {
+
+    // load up cb only the first time
+    callback = typeof callback == 'undefined' ? cb : callback;
+    skipIt = typeof skipIt == 'undefined' ? skip : skipIt;
+
+    if (typeof details == 'undefined') {
+
+      // details
+      Artwork.getDetail(artworkId, Artwork.sameArt);
+
+    } else if (typeof sameArt == 'undefined') {
+
+      // images for same art
+      Artwork.getSameArt(artworkId, 's3', skipIt, Artwork.sameArt);
+
+    } else {
+
+      // cb
+      callback(null, {'sameArt': sameArt});
+
+      // clean up
+      callback = undefined;
+      skipIt = undefined;
+      sameArt = undefined;
     }
 
+  }
 
-    Artwork.remoteMethod(
-        'detailView',
-        {
-            http: { path: '/detailview', verb: 'get'},
-            accepts: {arg: 'id', type: 'string', http: { source: 'query'}, required: true, description:["Valid Artwork id"] },
-            returns: {arg: 'artwork', type: 'array'}
-        }
-    );
+  Artwork.remoteMethod(
+    'detailView',
+    {
+        http: { path: '/detailView', verb: 'get'},
+        accepts: {arg: 'id', type: 'string', http: { source: 'query'}, required: true, description:["Valid Artwork id"] },
+        returns: {arg: 'artwork', type: 'array'}
+    }
+  );
 
-    Artwork.getDetailsImgs = function (artworkId) {
+  Artwork.remoteMethod(
+    'sameArt',
+    {
+      http: { path: '/sameArt', verb: 'get'},
+      accepts:[
+        {arg: 'id', type: 'string', http: { source: 'query'}, required: true, description:["Valid Artwork Id"] },
+        {arg: 'skip', type: 'number', http: { source: 'query'}, required: true, description:["Valid number to skip"] }
+      ],
+      returns: {arg: 'artwork', type: 'array'}
+    }
+  );
 
-        if (typeof details == 'undefined') {
+  Artwork.getDetailsImgs = function (artworkId) {
 
-            // details
-            Artwork.getDetail(artworkId);
+    if (typeof details == 'undefined') {
 
-        } else if (typeof imgs == 'undefined') {
+      // details
+      Artwork.getDetail(artworkId, Artwork.getDetailsImgs);
 
-            // images
-            Artwork.getImages(artworkId, 's3');
+    } else if (typeof imgs == 'undefined') {
 
-        } else {
+      // images
+      Artwork.getImages(artworkId, 's3');
 
-            // cb
-            callback(null, {'details': details, 'images': imgs});
+    } else if (typeof sameArtist == 'undefined') {
 
-            // clean up
-            details = undefined;
-            imgs = undefined;
-            callback = undefined;
+      //  images for same artist
+      Artwork.getSameArtist(artworkId, 's3');
 
-        }
+   } else if (typeof sameArt == 'undefined') {
 
-    };
+      // images for same art
+     Artwork.getSameArt(artworkId, 's3', 0, Artwork.getDetailsImgs);
 
-    Artwork.getDetail = function (artworkId) {
+    } else {
 
-        Artwork.findById( artworkId, function (err, instance){
+      // cb
+      callback(null, {'details': details, 'images': imgs, 'sameArtist': sameArtist, 'sameArt': sameArt});
 
-            // error condition... abort.
-            if (err) {
-                console.log(err.message);
-                return callback(err);
-            }
+      // clean up
+      details = undefined;
+      imgs = undefined;
+      callback = undefined;
+      sameArt = undefined;
+      sameArtist = undefined;
+    }
+  };
 
-           details = instance;
+  Artwork.getDetail = function (artworkId, cb) {
 
-            Artwork.getDetailsImgs(artworkId);
+    // filter/inclusion
+    var filter = {fields:['id', 'status', 'media', 'name', 'width', 'depth', 'edition', 'editionCount', 'length', 'salesPrice', 'artistId', 'galleryId']};
 
-        });
-    };
+    Artwork.findById( artworkId, filter, function (err, instance){
 
-   Artwork.getImages = function (artworkId, size) {
-        var Images = Artwork.app.models.Image;
-        filter = { "where": {"imageId":artworkId, "size":size} };
-        Images.find( filter, function (err, instance){
-            // error condition... abort.
-            if (err) {
-                console.log(err.message);
-                return callback(err);
-            }
+      // error condition... abort.
+      if (err) {
+          console.log(err.message);
+          return callback(err);
+      }
 
-            imgs = instance;
+      details = instance;
 
-            Artwork.getDetailsImgs(artworkId);
+      cb(artworkId);
 
-        });
+    });
+  };
 
-    };
+ Artwork.getImages = function (artworkId, size) {
 
+    var Images = Artwork.app.models.Image;
+
+    var filter = { "where": {"imageId":artworkId, "size":size} };
+
+    Images.find( filter, function (err, instance){
+
+      // error condition... abort.
+      if (err) {
+          console.log(err.message);
+          return callback(err);
+      }
+
+      imgs = instance;
+
+      Artwork.getDetailsImgs(artworkId);
+
+    });
+
+  };
+
+  Artwork.getSameArtist = function (artworkId, size) {
+
+    // abort if not available
+    if (typeof details == 'undefined') return;
+
+   // filter/inclusion
+    var filter = {fields:['id'], where: { artistId: details.artistId}, include:{ relation: 'images', scope : { fields:['url'], where: {'size': size} }}};
+
+    Artwork.find( filter, function (err, instance){
+
+      // error condition... abort.
+      if (err) {
+          console.log(err.message);
+          return callback(err);
+      }
+
+      sameArtist = instance;
+
+      Artwork.getDetailsImgs(artworkId);
+
+    });
+
+  };
+
+  Artwork.getSameArt = function (artworkId, size, skipDocs, cb) {
+
+    // abort if not available
+    if (typeof details == 'undefined') return;
+
+    // filter/inclusion
+    var filter = {fields:['id'], skip:skipDocs, where: { media: details.media }, include:{ relation: 'images', scope : { fields:['url'], where: {'size': size} }}, limit:4 };
+
+    Artwork.find( filter, function (err, instance){
+
+      // error condition... abort.
+      if (err) {
+        console.log(err.message);
+        return callback(err);
+      }
+
+      sameArt = instance;
+
+      cb(artworkId);
+
+    });
+
+  };
 
 };
--- common/models/collection.js
+++ common/models/collection.js
@@ -1,3 +1,97 @@
 module.exports = function(Collection) {
 
+  // hold
+  var uCollection = undefined;
+  var gCollection = undefined;
+  var callback = undefined;
+  var skipIt = undefined;
+
+  Collection.findCollection = function(userId, cb) {
+
+    console.log(userId);
+    // load up cb only the first time
+    callback = typeof callback == 'undefined' ? cb : callback;
+
+    Collection.getCollections(userId);
+
+  }
+
+  Collection.remoteMethod(
+    'findCollection',
+    {
+      http: { path: '/', verb: 'get'},
+      accepts: {arg: 'id', type: 'string', http: { source: 'query'}, required: true, description:["Valid user id"] },
+      returns: {arg: 'collection', type: 'array'}
+    }
+  );
+
+  Collection.getCollections = function(userId){
+
+    if (typeof uCollection == 'undefined') {
+
+      // user collections
+      Collection.getUserCollection(userId, Collection.getCollections);
+
+    } else if (typeof gCollection == 'undefined') {
+
+      // global collections
+      Collection.getGlobalCollection(Collection.getCollections);
+
+    } else {
+
+      // cb
+      callback(null, {user: uCollection, global: gCollection});
+
+      // clean up
+      uCollection = undefined;
+      gCollection = undefined;
+      callback = undefined;
+      skipIt = undefined;
+
+    }
+  }
+
+  Collection.getUserCollection = function(uid, cb){
+
+    //console.log(Collection.app.models);
+    var uc = Collection.app.models['user-collection'];
+
+    var filter = {fields:['recommended', 'wishlist', 'nearBy'], where: { userId: uid}};
+
+    uc.find(filter, function (err, instance){
+
+      // error condition... abort.
+      if (err) {
+        console.log(err.message);
+        return callback(err);
+      }
+
+      uCollection = instance;
+
+      cb(uid);
+
+    });
+  }
+
+  Collection.getGlobalCollection = function(cb){
+
+    var gc = Collection.app.models['global-collection'];
+
+    var filter = {fields:['featuredArtists', 'trending', 'newlyCurated']};
+
+    gc.find(filter, function (err, instance){
+
+      // error condition... abort.
+      if (err) {
+        console.log(err.message);
+        return callback(err);
+      }
+
+      gCollection = instance;
+
+      cb();
+
+    });
+  }
+
 };
